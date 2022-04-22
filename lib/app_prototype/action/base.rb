dd174be6612a32bd979c73b2229778d7f7e94949
Benjamin Klotz <benny.klotz92@gmail.com>
2022-04-22 20:27:33 +0200

    Initial commit

---
+++ lib/app_prototype/action/base.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "hanami/action"
+
+module AppPrototype
+  module Action
+    class Base < Hanami::Action
+    end
+  end
+end
---
+++ slices/main/lib/view/base.rb
@@ -0,0 +1,11 @@
+# auto_register: false
+# frozen_string_literal: true
+
+require "app_prototype/view/base"
+
+module Main
+  module View
+    class Base < AppPrototype::View::Base
+    end
+  end
+end
