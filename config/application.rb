459ccac5b015201f893af682c49057347ab27e43
Benjamin <benny.klotz92@gmail.com>
2022-04-23 00:27:10 +0200

    Setup bulma, try out binding.irb, npm run start not working yet

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,25 @@ module Timetracker
       secret: settings.session_secret,
       expire_after: 60 * 60 * 24 * 365 # 1 year
     }
+
+    config.actions.default_headers = {
+      "X-Frame-Options" => "DENY",
+      "X-Content-Type-Options" => "nosniff",
+      "X-XSS-Protection" => "1; mode=block",
+      "Content-Security-Policy" => \
+      "base-uri 'self'; " \
+      "child-src 'self'; " \
+      "connect-src 'self'; " \
+      "default-src 'none'; " \
+      "font-src 'self'; " \
+      "form-action 'self'; " \
+      "frame-ancestors 'self'; " \
+      "frame-src 'self'; " \
+      "img-src 'self' https: data:; " \
+      "media-src 'self'; " \
+      "object-src 'none'; " \
+      "script-src 'localhost'; " \
+      "style-src * 'unsafe-inline'"
+    }
   end
 end
---
+++ slices/main/views/blog/articles.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Article = Struct.new(:title)
+
+module Main
+  module Views
+    module Blog
+      class Articles < View::Base
+        expose :articles do
+          binding.irb
+          %w[article1 article2 article3].map do |title|
+            Article.new(title)
+          end
+        end
+      end
+    end
+  end
+end
