22480bba957e1812e12028e2607fc1c580381052
Benjamin <benny.klotz92@gmail.com>
2022-05-04 00:22:07 +0200

    Convert items index template to erb, get assets working

--- config/application.rb
+++ config/application.rb
@@ -10,24 +10,28 @@ module Timetracker
       expire_after: 60 * 60 * 24 * 365 # 1 year
     }
 
-    config.actions.default_headers = {
-      "X-Frame-Options" => "DENY",
-      "X-Content-Type-Options" => "nosniff",
-      "X-XSS-Protection" => "1; mode=block",
-      "Content-Security-Policy" => \
-      "base-uri 'self'; " \
-      "child-src 'self'; " \
-      "connect-src 'self'; " \
-      "default-src 'none'; " \
-      "font-src 'self'; " \
-      "form-action 'self'; " \
-      "frame-ancestors 'self'; " \
-      "frame-src 'self'; " \
-      "img-src 'self' https: data:; " \
-      "media-src 'self'; " \
-      "object-src 'none'; " \
-      "script-src 'localhost'; " \
-      "style-src * 'unsafe-inline'"
-    }
+    config.actions.content_security_policy[:default_src] = "'self' http://localhost:8080"
+    # config.actions.content_security_policy[:style_src_elem] = "'self' 'unsafe-inline' https: http://localhost:8080"
+    # config.actions.content_security_policy[:script_src_elem] = "'self' http://localhost:8080"
+
+    # config.actions.default_headers = {
+    #   "X-Frame-Options" => "DENY",
+    #   "X-Content-Type-Options" => "nosniff",
+    #   "X-XSS-Protection" => "1; mode=block",
+    #   "Content-Security-Policy" => \
+    #   "base-uri 'self'; " \
+    #   "child-src 'self'; " \
+    #   "connect-src 'self'; " \
+    #   "default-src 'none'; " \
+    #   "font-src 'self'; " \
+    #   "form-action 'self'; " \
+    #   "frame-ancestors 'self'; " \
+    #   "frame-src 'self'; " \
+    #   "img-src 'self' https: data:; " \
+    #   "media-src 'self'; " \
+    #   "object-src 'none'; " \
+    #   "script-src 'localhost'; " \
+    #   "style-src * 'unsafe-inline'"
+    # }
   end
 end
